3fe5ff5b3636ebea97fa59bb1badf904a8c7c1df
blndl <jumarie.pro@gmail.com>
2024-01-31 12:18:18 +0100

    1.2 loop added

--- exo.py
+++ exo.py
@@ -1,5 +1,8 @@
-nbr = input("Entrez un nombre\n")
-i = 0
-while i <= 10:
-    print(int(nbr) * i)
-    i += 1
+while 1:
+    nbr = input("Entrez un nombre pour lancer le programme ou autre chose pour fermer\n")
+    if nbr.isnumeric() == False:
+        break
+    i = 0
+    while i <= 10:
+        print(int(nbr) * i)
+        i += 1
